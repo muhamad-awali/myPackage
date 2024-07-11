59803f636a30edee103e99ef4390639a2bddc426
putraawali <muhamad.awali@ruparupa.com>
2024-07-11 13:17:16 +0700

    Add sum function

---
+++ calculate.go
@@ -0,0 +1,8 @@
+package mypackage
+
+import "fmt"
+
+func Sum(a, b int) int {
+	fmt.Printf("Sum %d + %d \n", a, b)
+	return a + b
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/muhamad-awali/myPackage
+
+go 1.22.3
